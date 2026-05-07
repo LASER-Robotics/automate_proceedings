f0b3e418e4f943e4999e5e8eb93009c31e857b61
davihdsantos <davihenriqueds@gmail.com>
2026-05-07 12:18:32 -0300

    get folder path from argument

--- camera_ready_check.py
+++ camera_ready_check.py
@@ -1,4 +1,12 @@
+"""
+Input: Folder with all the pdfs named by ID (001.pdf, 002.pdf, etc)
+
+Usage:
+    python3 camera_ready.py --path camera_ready_folder_path
+"""
+
 # Imports
+import argparse
 from PyPDF2 import PdfReader
 from pathlib import Path
 import pandas as pd
@@ -8,15 +16,11 @@ from pdf_metadata_extractor import process_pdf
 from scripts.contagem_de_paginas import count_page
 from scripts.ieee_string_copyright import search_text
 
-# Configs
-# Folder with all the pdfs named by ID (001.pdf, 002.pdf, etc)
-PATH_1 = "./01 - PDF Artigos CMT/"
-
 count = 1
 
-def processar():
+def processar(PATH):
     global count
-    folder = Path(PATH_1)
+    folder = Path(PATH)
     files = [f.name for f in folder.iterdir() if f.is_file()]
     files = sorted(files)
 
@@ -25,7 +29,7 @@ def processar():
         writer.writerow(["cmt_id", "title_pdf", "authors_pdf", "#_pages", "copyright", "ieee_compliace", "#_first_page", "#_last_page"])
 
         for f in files:                        
-            caminho = f"{PATH_1}{f}"
+            caminho = f"{PATH}{f}"
             folder_obj = Path(caminho)
             extrator = process_pdf(folder_obj)
 
@@ -39,8 +43,11 @@ def processar():
 
             writer.writerow([f, extrator["pdf_title"], extrator["pdf_authors"], pages, text_found, ieee_complance_check, pagina_inicial, pagina_final])
             
-    print(f"csv dados_dos_artigos.csv criado com sucesso!")
+    print(f"OUTPUT: camera_ready_report.csv")
 
 
 if __name__ == "__main__":
-    processar()
+    parser = argparse.ArgumentParser(description="Camera-Ready Verification Tool")
+    parser.add_argument("--path", default="./01 - PDF Artigos CMT/", help="Path to the folder with the Camera-Ready Files Named 001.pdf, 002.pdf, etc")
+    args = parser.parse_args()
+    processar(args.path)
